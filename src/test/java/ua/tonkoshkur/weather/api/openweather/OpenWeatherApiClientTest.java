dc6edc95877b2563228eba0ae8a95d1582f362d7
Ilya Tonkoshkur <54292571+tonkoshkur@users.noreply.github.com>
2024-06-13 18:47:17 +0200

    test(api): enhance test coverage by overriding OpenWeatherApiClient tests

--- src/test/java/ua/tonkoshkur/weather/api/openweather/OpenWeatherApiClientTest.java
+++ src/test/java/ua/tonkoshkur/weather/api/openweather/OpenWeatherApiClientTest.java
@@ -3,114 +3,121 @@ package ua.tonkoshkur.weather.api.openweather;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import ua.tonkoshkur.weather.api.WeatherApiException;
 import ua.tonkoshkur.weather.api.WeatherDto;
-import ua.tonkoshkur.weather.api.WeatherHttpClient;
+import ua.tonkoshkur.weather.api.openweather.dto.geo.GeoResponse;
+import ua.tonkoshkur.weather.api.openweather.dto.weather.MainDto;
+import ua.tonkoshkur.weather.api.openweather.dto.weather.WeatherResponse;
+import ua.tonkoshkur.weather.api.openweather.dto.weather.WindDto;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.spy;
 
 @ExtendWith(MockitoExtension.class)
 class OpenWeatherApiClientTest {
 
-    private static final String CITY = "London";
-    private static final String FIND_GEO_BY_CITY_RESPONSE = """
-            [
-                {
-                    "name":"London",
-                    "local_names": {
-                        "en":"London",
-                        "uk":"Лондон",
-                        "ru":"Лондон"
-                    },
-                    "lat":51.5073219,
-                    "lon":-0.1276474,
-                    "country":"GB",
-                    "state":"England"
-                }
-            ]
-            """;
-    private static final String FIND_BY_CITY_RESPONSE = """
-            {
-                "coord": {
-                    "lon": -0.1257,
-                    "lat": 51.5085
-                },
-                "weather": [
-                    {
-                        "id": 801,
-                        "main": "Clouds",
-                        "description": "few clouds",
-                        "icon": "02d"
-                    }
-                ],
-                "base": "stations",
-                "main": {
-                    "temp": 18.16,
-                    "feels_like": 17.34,
-                    "temp_min": 16.11,
-                    "temp_max": 19.72,
-                    "pressure": 1017,
-                    "humidity": 50
-                },
-                "visibility": 10000,
-                "wind": {
-                    "speed": 3.09,
-                    "deg": 250
-                },
-                "clouds": {
-                    "all": 20
-                },
-                "dt": 1717682152,
-                "sys": {
-                    "type": 2,
-                    "id": 2075535,
-                    "country": "GB",
-                    "sunrise": 1717645535,
-                    "sunset": 1717704791
-                },
-                "timezone": 3600,
-                "id": 2643743,
-                "name": "London",
-                "cod": 200
-            }
-            """;
-
-    private static WeatherHttpClient httpClient;
     private static OpenWeatherApiClient openWeatherApiClient;
+    private static OpenWeatherApi openWeatherApi;
+    private static GeoResponse geoResponse = new GeoResponse();
+    private static WeatherResponse weatherResponse = new WeatherResponse();
+    private static WeatherDto expectedWeather = WeatherDto.builder().build();
 
     @BeforeAll
     static void setUp() {
-        httpClient = Mockito.mock(WeatherHttpClient.class);
-        openWeatherApiClient = new OpenWeatherApiClient(httpClient, "");
+        openWeatherApi = spy(new OpenWeatherApi(null, ""));
+        openWeatherApiClient = new OpenWeatherApiClient(openWeatherApi);
+        setUpGeoResponse();
+        setUpWeatherResponse();
+        setUpExpectedWeatherDto();
+    }
+
+    private static void setUpGeoResponse() {
+        geoResponse = new GeoResponse();
+        geoResponse.setName("London");
+        geoResponse.setLat(new BigDecimal("89.999"));
+        geoResponse.setLon(new BigDecimal("179.999"));
+        geoResponse.setState("England");
+        geoResponse.setCountry("GB");
+    }
+
+    private static void setUpWeatherResponse() {
+        weatherResponse = new WeatherResponse();
+        weatherResponse.setVisibilityMeters(10000);
+
+        MainDto main = new MainDto();
+        main.setTemp(11.22);
+        main.setFeelsLike(22.33);
+        main.setTempMin(33.44);
+        main.setTempMax(44.55);
+        main.setHumidity(10);
+        weatherResponse.setMain(main);
+
+        ua.tonkoshkur.weather.api.openweather.dto.weather.WeatherDto weather
+                = new ua.tonkoshkur.weather.api.openweather.dto.weather.WeatherDto();
+        weather.setMain("Clouds");
+        weather.setDescription("few clouds");
+        weather.setIcon("02d");
+        weatherResponse.setWeather(List.of(weather));
+
+        WindDto wind = new WindDto();
+        wind.setSpeed(55.66);
+        weatherResponse.setWind(wind);
+    }
+
+    private static void setUpExpectedWeatherDto() {
+        ua.tonkoshkur.weather.api.openweather.dto.weather.WeatherDto weatherResponseWeather
+                = weatherResponse.getWeather().getFirst();
+        MainDto main = weatherResponse.getMain();
+        expectedWeather = WeatherDto.builder()
+                .city(geoResponse.getName())
+                .state(geoResponse.getState())
+                .countryCode(geoResponse.getCountry())
+                .latitude(geoResponse.getLat())
+                .longitude(geoResponse.getLon())
+                .name(weatherResponseWeather.getMain())
+                .description(weatherResponseWeather.getDescription())
+                .iconUrl(String.format(openWeatherApi.getIconUrlFormat(), weatherResponseWeather.getIcon()))
+                .temperature(main.getTemp().intValue())
+                .feelsLike(main.getFeelsLike().intValue())
+                .minTemperature(main.getTempMin())
+                .maxTemperature(main.getTempMax())
+                .humidity(main.getHumidity())
+                .windSpeed(weatherResponse.getWind().getSpeed())
+                .visibility(weatherResponse.getVisibilityMeters() / 1000)
+                .build();
     }
 
     @Test
-    void givenCityName_whenFindAllByCity_thenReturnCityWeather() {
-        when(httpClient.sendRequest(anyString())).thenReturn(FIND_GEO_BY_CITY_RESPONSE, FIND_BY_CITY_RESPONSE);
+    void findAllByCity_withGeoResponseCity_returnsCorrectWeatherDto() {
+        doReturn(List.of(geoResponse)).when(openWeatherApi).findGeoByCity(anyString());
+        doReturn(weatherResponse).when(openWeatherApi).findWeatherByCoordinates(any(), any());
 
-        List<WeatherDto> weather = openWeatherApiClient.findAllByCity(CITY);
+        List<WeatherDto> weather = openWeatherApiClient.findAllByCity(geoResponse.getName());
 
         assertThat(weather)
                 .singleElement()
-                .hasNoNullFieldsOrProperties()
-                .satisfies(w -> {
-                    assertThat(w.getCity()).isEqualTo(CITY);
-                    assertThat(w.getCountryCode()).isEqualTo("GB");
-                    assertThat(w.getState()).isEqualTo("England");
-                    assertThat(w.getLatitude()).isEqualTo("51.5073219");
-                    assertThat(w.getLongitude()).isEqualTo("-0.1276474");
-                });
+                .usingRecursiveComparison()
+                .isEqualTo(expectedWeather);
     }
 
     @Test
-    void givenNotValidResponse_whenFindAllByCity_thenThrowWeatherApiException() {
-        assertThrows(WeatherApiException.class, () -> openWeatherApiClient.findAllByCity(CITY));
+    void findAllByCoordinates_withGeoResponseCoordinates_returnsCorrectWeatherDto() {
+        doReturn(Optional.of(geoResponse)).when(openWeatherApi).findGeoByCoordinates(any(), any());
+        doReturn(weatherResponse).when(openWeatherApi).findWeatherByCoordinates(any(), any());
+
+        Optional<WeatherDto> weather = openWeatherApiClient.findByCoordinates(geoResponse.getLat(), geoResponse.getLon());
+
+        assertThat(weather)
+                .isPresent()
+                .get()
+                .usingRecursiveComparison()
+                .isEqualTo(expectedWeather);
     }
 }
