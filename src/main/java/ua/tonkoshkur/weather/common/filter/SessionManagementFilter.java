cd7f6070ae89bb977edd13fed6e85dc7186c261c
Ilya Tonkoshkur <54292571+tonkoshkur@users.noreply.github.com>
2024-06-14 14:36:21 +0200

    refactor(filter): rename AddSessionUserFilter to SessionManagementFilter

--- src/main/java/ua/tonkoshkur/weather/common/filter/SessionManagementFilter.java
+++ src/main/java/ua/tonkoshkur/weather/common/filter/SessionManagementFilter.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 import java.time.LocalDateTime;
 
 @WebFilter("/*")
-public class AddSessionUserFilter implements Filter {
+public class SessionManagementFilter implements Filter {
 
     private SessionDao sessionDao;
 
